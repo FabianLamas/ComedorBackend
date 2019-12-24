98c095caed70a8fe9434fffa672e61588ea90ccf
flamas <flamas@tgv.com.ar>
2019-12-24 14:53:57 -0300

    fin api

--- Sistema.Web/Controllers/ConsumosHistsController.cs
+++ Sistema.Web/Controllers/ConsumosHistsController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
@@ -47,101 +48,39 @@ namespace Sistema.Web.Controllers
             });
         }
 
-        // GET: api/ConsumosHists
-        [HttpGet]
-        public IEnumerable<ConsumoHist> GetConsumosHists()
+        // GET: api//ConsumosHists/buscarConsumos/desde/hasta/centro
+        [HttpGet("[action]/{desde}/{hasta}/{centro?}")]
+        public async Task<IEnumerable<ReporteViewModel>> BuscarConsumos([FromRoute] string desde, [FromRoute] string hasta, [FromRoute] string centro)
         {
-            return _context.ConsumosHists;
-        }
 
-        // GET: api/ConsumosHists/5
-        [HttpGet("{id}")]
-        public async Task<IActionResult> GetConsumoHist([FromRoute] int id)
-        {
-            if (!ModelState.IsValid)
+            DateTime _desde = DateTime.ParseExact(desde, "yyyyMMdd", CultureInfo.InvariantCulture);
+            DateTime _hasta = DateTime.ParseExact(hasta, "yyyyMMdd", CultureInfo.InvariantCulture);
+
+            var consumo = await _context.ConsumosHists.ToListAsync();
+
+            if (centro is null)
             {
-                return BadRequest(ModelState);
-            }
-
-            var consumoHist = await _context.ConsumosHists.FindAsync(id);
-
-            if (consumoHist == null)
+                consumo = await _context.ConsumosHists.Where(c =>
+                (DateTime.ParseExact(c.Fecha_consumo, "yyyyMMdd", CultureInfo.InvariantCulture) > _desde)
+                && (DateTime.ParseExact(c.Fecha_consumo, "yyyyMMdd", CultureInfo.InvariantCulture) < _hasta) && (DateTime.ParseExact(c.Fecha_consumo, "yyyyMMdd", CultureInfo.InvariantCulture) < _hasta)).ToListAsync();
+            }else
             {
-                return NotFound();
+                consumo = await _context.ConsumosHists.Where(c =>
+                (DateTime.ParseExact(c.Fecha_consumo, "yyyyMMdd", CultureInfo.InvariantCulture) > _desde)
+                && (DateTime.ParseExact(c.Fecha_consumo, "yyyyMMdd", CultureInfo.InvariantCulture) < _hasta) && (c.CentroCosto == centro)).ToListAsync();
             }
+            
 
-            return Ok(consumoHist);
-        }
-
-        // PUT: api/ConsumosHists/5
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutConsumoHist([FromRoute] int id, [FromBody] ConsumoHist consumoHist)
-        {
-            if (!ModelState.IsValid)
+            return consumo.Select(c => new ReporteViewModel
             {
-                return BadRequest(ModelState);
-            }
-
-            if (id != consumoHist.Id)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(consumoHist).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!ConsumoHistExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
-        // POST: api/ConsumosHists
-        [HttpPost]
-        public async Task<IActionResult> PostConsumoHist([FromBody] ConsumoHist consumoHist)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            _context.ConsumosHists.Add(consumoHist);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetConsumoHist", new { id = consumoHist.Id }, consumoHist);
-        }
-
-        // DELETE: api/ConsumosHists/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteConsumoHist([FromRoute] int id)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            var consumoHist = await _context.ConsumosHists.FindAsync(id);
-            if (consumoHist == null)
-            {
-                return NotFound();
-            }
-
-            _context.ConsumosHists.Remove(consumoHist);
-            await _context.SaveChangesAsync();
-
-            return Ok(consumoHist);
+                Id = c.Id,
+                Numero_tarjeta = c.Numero_tarjeta,
+                Nombre = c.Nombre,
+                CentroCosto = c.CentroCosto,
+                Numero_turno = c.Numero_turno,
+                Fecha_Consumo = c.Fecha_consumo,
+                Hora = c.Hora
+            });
         }
 
         private bool ConsumoHistExists(int id)
---
+++ Sistema.Web/Modelos/ReporteViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sistema.Web.Modelos
+{
+    public class ReporteViewModel
+    {
+        public int Id { get; set; }
+        public string Numero_tarjeta { get; set; }
+        public string Nombre { get; set; }
+        public string CentroCosto { get; set; }
+        public decimal Numero_turno { get; set; }
+        public string Fecha_Consumo { get; set; }
+        public string Hora { get; set; }
+    }
+}
